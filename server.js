258b88bcc659baf4d58610a4f78efc00aa06d39b
gjdickens <geeregg@gmail.com>
2016-10-24 08:13:48 +0100

    added ip address

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ router.route('/')
     var osLong = regExpOs.exec(req.headers['user-agent'])[1];
     var language = req.headers['accept-language'];
     var languageArr = language.split(";");
-    res.json({"total": req.headers, "ipaddress": req.headers['host'], "software": osLong, "language": languageArr[0]});
+    res.json({"ipaddress": req.connection.remoteAddress, "software": osLong, "language": languageArr[0]});
   });
 
 // Register Routes
